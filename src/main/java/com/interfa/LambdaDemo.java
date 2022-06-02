8faac1eb46039d7b696460519bb2eafb2f1abcd5
AniShirke <shirkeaniket19999@gmail.com>
2022-06-02 10:50:24 +0530

    commit

--- src/main/java/com/interfa/LambdaDemo.java
+++ src/main/java/com/interfa/LambdaDemo.java
@@ -3,6 +3,8 @@ package com.interfa;
 public class LambdaDemo {
 
 	public static void main(String[] args) {
+		
+		//Important
 
 		// object can't be created for an interface 
 //		SomeInterface obj = new SomeInterface();
